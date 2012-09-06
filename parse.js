4f51a46a48e659d92bb7f45ff703d5fc3b7e791a
Liam Davis <liam.davis@trezoid.com>
2012-09-06 18:21:43 +1000

    Extended the demo extensively

--- parse.js
+++ parse.js
@@ -5,17 +5,17 @@ parser.prototype.csv = function(csvName, callback)
 }
 parsecsv = function(data){
 	var dat = [];
-	var blobLines = data.split('\r\n');
+	var blobLines = data.split("\r");
 	var headers = blobLines[0].toLowerCase().split(',');
 	for(var i = 1; i < blobLines.length; i++)
 	{
 		var lineArr = blobLines[i].split(',');
 		var line = {};
-		if(lineArr.length === headers.length)
+		if(lineArr.length > 1)
 		{
 			for(var l = 0; l < lineArr.length; l++)
 			{
-				line[headers[l]] = lineArr[l];
+				line[headers[l]] = (lineArr[l].length > 0) ? lineArr[l] : " ";
 			}
 			dat[i - 1] = line;
 		}
--- test.js
+++ test.js
@@ -1,13 +1,65 @@
+var wasps;
+var schools;
+var play;
 (function(){
  	var p = new parser();
-	var dat = p.csv("Location of European Wasps Nests.csv", processData);
-})();
+ 	p.csv("Location of European Wasps Nests.csv", function(data){ wasps = data});
+ 	p.csv("ACT - PLAYGROUNDS - V_NM_PLGR_IAMS15092009.csv", function(data){play = data});
+	p.csv("schools.csv", function(data){schools = data; processData();});
+ })();
 
-function processData(data)
+function processData()
 {
-	for(var i = 0; i < data.length; i++)
+	var mostWasps = wasps[2]
+		for(var i = 3; i < wasps.length; i++)
+		{
+			if((wasps[i]))
+			{
+				if((wasps[i])["suburb"].toLowerCase() != "total")
+				{
+					if(parseInt((wasps[i])["number of nests"]) > parseInt(mostWasps["number of nests"]))
+						mostWasps = wasps[i];
+				}
+			}
+		}
+	var waspSchool = [];
+	for(var i = 0; i < schools.length; i++)
 	{
-		if((data[i])["year"] === "2011")
-			console.log("There were " + (data[i])["number of nests"] + " nests found in " + (data[i])["suburb"]);
+		if((schools[i])["l_addr_2"].toLowerCase() === mostWasps["suburb"].toLowerCase())
+		{
+				waspSchool[waspSchool.length] = schools[i];
+		}
 	}
+
+	var holder = document.createElement("div");
+	holder.innerHTML = 'The following schools (located in ' + mostWasps["suburb"] + ') were near the highest number of wasp nests in ' + mostWasps["year"] + ':<br/><ul id="list">'
+		document.body.appendChild(holder);
+	var list = document.getElementById('list');
+	for(var i = 0; i < waspSchool.length; i++)
+	{
+		var ul = document.createElement("li");
+		ul.innerHTML = (waspSchool[i])["cmps_name"];
+		list.appendChild(ul);
+	}
+
+
+	var playCounts = {};
+	var mostPlayCount = 0;
+	var mostPlay;
+	for(var i = 0; i < play.length; i++)
+	{
+		if(playCounts[(play[i])["contract_unit"]])
+		{
+			playCounts[(play[i])["contract_unit"]] += 1;
+			if(playCounts[(play[i])["contract_unit"]] > mostPlayCount)
+			{
+				mostPlay = play[i];
+				mostPlayCount = playCounts[(play[i])["contract_unit"]];
+			}
+		} else{ playCounts[(play[i])["contract_unit"]] = 1; }
+	}
+	
+	var holder2 = document.createElement('div');
+	holder2.innerHTML ="The suburb with the most playgrounds is " + mostPlay["contract_unit"];
+	document.body.appendChild(holder2);
 }
