1bdce9ba05041d9ab7c12b2bd1becf00f14f4857
Peter Monus <pete.monus@gmail.com>
2012-10-08 21:40:22 +1100

    Attempt at geolocation, other minor changes

--- test.js
+++ test.js
@@ -3,25 +3,12 @@
  	parse.csv("BP.CSV", function(data){petrolLocations(data)}, ["nsw"]);
 })();
 
-function petrolLocations(petrol)
-{
-	var holder = document.createElement("div");
-	holder.innerHTML = 'Locations of all caltex stores in australia:<br/><table id="list"><tr><th>store ID</th><th>Store Name</th><th>State</th><th>Street Address</th><th>Lat</th><th>Long</th></tr>';
-	document.body.appendChild(holder);
-	var list = document.getElementById("list");
-	for(var i = 0; i < petrol.length; i++)
-	{
-		current = petrol[i];
-		var row = document.createElement('tr');
-		row.innerHTML = '<td>'+i+'</td><td>'+current["name"] + " " + current["town"] + "</td><td>" + current["state"]+ "</td><td>" + current["street"] + "</td>";
-		list.appendChild(row);
-	}
-}
-
 	var markers = [];
 	var stations = [];
+	var content = [];
 	var iterator = 0;
 	var map;
+    var image = "";
 	
 (function(){
  	parse.csv("CaltexSites_AU.CSV", function(data){initialize(data)},[""]);
@@ -41,8 +28,10 @@ function initialize(petrol)
 	for(var i = 0; i < petrol.length; i++)
 	{
 		current = petrol[i];
-		stations.push(new google.maps.LatLng(current["latitude"], current["longitude"]));			
+		stations.push(new google.maps.LatLng(current["latitude"], current["longitude"]));
 	}
+	
+	image = "images/marker_caltex.png";
 	drop();
 }
 
@@ -51,8 +40,6 @@ function drop() {
 		addMarker();		
 	}
 }
-
-var image = "images/marker_caltex.png";
 
 function addMarker() {
 	markers.push(new google.maps.Marker({
