93b7ee6b73470de3577619051f16562cfefb4f5e
Liam Davis <liam.davis@trezoid.com>
2012-10-09 15:45:26 +1100

    more refactoring.

--- maps.js
+++ maps.js
@@ -1,4 +1,16 @@
-function drop(stations) {
+function initMap()
+{
+	var mapOptions = 
+	{
+		center: new google.maps.LatLng(userLoc["lat"], userLoc["long"]),
+		zoom: 14,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	return new google.maps.Map(document.getElementById("map_canvas"),
+	mapOptions);
+}
+
+function drop(stations, map, image) {
 	for (var i = 0; i < stations.length; i++) {
 		markers.push(new google.maps.Marker({
 			position: stations[i],
--- test.js
+++ test.js
@@ -1,9 +1,6 @@
 	var markers = [];
-	var content = [];
-	var map;
 	var userLoc = {};
 	(function(){navigator.geolocation.getCurrentPosition(initLoc);})();
-    var image = "";
 	
 function initLoc(position){
 	userLoc["lat"] = position.coords.latitude;
@@ -13,15 +10,8 @@ function initLoc(position){
 		
 function initialize(petrol) 
 {
-	var mapOptions = 
-	{
-		center: new google.maps.LatLng(userLoc["lat"], userLoc["long"]),
-		zoom: 14,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	map = new google.maps.Map(document.getElementById("map_canvas"),
-	mapOptions);
-	
+	var map = initMap();	
+
 	var stations = [];
 	for(var i = 0; i < petrol.length; i++)
 	{
@@ -29,15 +19,13 @@ function initialize(petrol)
 		//console.log(current["latitude"]);
 		if(distance(current["latitude"],current["longitude"]) < 20)
 		{
-		stations.push(new google.maps.LatLng(current["latitude"], current["longitude"]));			
+			stations.push(new google.maps.LatLng(current["latitude"], current["longitude"]));			
 		}
 	}
-	
-	image = "images/marker_caltex.png";
-	
-		google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
-			drop(stations);
-		});
 
+	var image = "images/marker_caltex.png";
+
+	google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
+			drop(stations, map, image);
+			});
 }
-
