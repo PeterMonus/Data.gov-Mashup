c4b3b5212554f47f6e348cb80c59791b9d512dba
Liam Davis <liam.davis@trezoid.com>
2012-11-06 16:53:00 +1100

    JS Validation fixes

--- js/main.js
+++ js/main.js
@@ -2,48 +2,48 @@ var userLoc = {};
 var pins;
 var map;
 
-$(document).ready(function() {
-		navigator.geolocation.getCurrentPosition(initLoc); //Initialise GeoLocation
-		map = initMap();//Load the map now. We'll move it later.
+$(document).ready(function() {	
+    navigator.geolocation.getCurrentPosition(initLoc); //Initialise GeoLocation
+    map = initMap();//Load the map now. We'll move it later.
 
-		/********************
-		 * The following code initiallises tab click for *all* tabs
-		 * Even ones we might add later.
-		 ********************/
-		$('.tab').each(function() {
-			$(this).click(function() {
-				selectTab(this);
-				});
-			});
+    /********************
+     * The following code initiallises tab click for *all* tabs
+     * Even ones we might add later.
+     ********************/
+    $('.tab').each(function() {
+      $(this).click(function() {
+        selectTab(this);
+        });
+      });
 
-		/****************
-		 * Initialise search button click
-		 ***************/
-		$('#search').click(function() {
-			clickSearch();
-			});
+    /****************
+     * Initialise search button click
+     ***************/
+    $('#search').click(function() {
+      clickSearch();
+      });
 
-		$('.comp').each(
-				function() {
-				$(this).click(
-					function() {
-					if (!($(this).is('#chkAllCompanies')) && $('#chkAllCompanies').is(':checked'))
-					{
-					$('#chkAllCompanies').prop('checked', '');
-					}
-					});
-				});
+    $('.comp').each(
+        function() {
+        $(this).click(
+          function() {
+          if (!($(this).is('#chkAllCompanies')) && $('#chkAllCompanies').is(':checked'))
+          {
+          $('#chkAllCompanies').prop('checked', '');
+          }
+          });
+        });
 
-		$('#chkAllCompanies').click(
-				function() {
-				if (!($(this).is(':checked')))
-				{
-				$('.comp').each(function() {$(this).prop('checked', 'on');});
-				} else {
+    $('#chkAllCompanies').click(
+        function() {
+        if (!($(this).is(':checked')))
+        {
+        $('.comp').each(function() {$(this).prop('checked', 'on');});
+        } else {
 
-				$('.comp').each(function() {$(this).prop('checked', '');});
-				}
-				});
+        $('.comp').each(function() {$(this).prop('checked', '');});
+        }
+        });
 
 
 });
@@ -52,55 +52,55 @@ $(document).ready(function() {
  * Callback function for loaded geolocation position
  **************************/
 function initLoc(position) {
-	userLoc['lat'] = position.coords.latitude;
-	userLoc['long'] = position.coords.longitude;
-	initCSV();
+  userLoc['lat'] = position.coords.latitude;
+  userLoc['long'] = position.coords.longitude;
+  initCSV();
 }
 
 function initCSV()
 {
-	var all = $('#chkAllCompanies').is(':checked');
-	var caltex = $('#chkCaltex').is(':checked');
-	var shell = $('#chkShell').is(':checked');
-	var BP = $('#chkBP').is(':checked');
-	var mobil = $('#chkMobil').is(':checked');
+  var all = $('#chkAllCompanies').is(':checked');
+  var caltex = $('#chkCaltex').is(':checked');
+  var shell = $('#chkShell').is(':checked');
+  var BP = $('#chkBP').is(':checked');
+  var mobil = $('#chkMobil').is(':checked');
 
-	var filter = {};
+  var filter = {};
 
-	if (!$('#chkAllFuel').is(':checked'))
-	{
-		$('.fuel').each(function() {
-				console.log($(this).prop('name'));
-				if ($(this).is(':checked'))
-				{
-				filter[$(this).prop('name').toLowerCase()] = true;
-				}
-				});
-	}
+  if (!$('#chkAllFuel').is(':checked'))
+  {
+    $('.fuel').each(function() {
+        console.log($(this).prop('name'));
+        if ($(this).is(':checked'))
+        {
+        filter[$(this).prop('name').toLowerCase()] = true;
+        }
+        });
+  }
 
-	if (!$('#chkService').is(':checked'))
-	{
-		$('.service').each(function() {
-				console.log($(this).prop('name'));
-				if ($(this).is(':checked'))
-				{
-				filter[$(this).prop('name').toLowerCase()] = true;
-				}
-				});
-	}
-	console.log(filter);
-	if (all || caltex) parse.csv('csv/caltex.csv', 
-				function(data) {initialize(data, 'images/marker_caltex.png')}, 
-				filter);
-	if (all || shell) parse.csv('csv/shell.csv', 
-			function(data) {initialize(data, 'images/marker_shell.png')}, 
-			filter);
-	if (all || BP) parse.csv('csv/BP.csv', 
-			function(data) {initialize(data, 'images/marker_bp.png')}, 
-			filter);
-	if (all || mobil) parse.csv('csv/mobil.csv', 
-			function(data) {initialize(data, 'images/marker_711.png')}, 
-			filter);
+  if (!$('#chkService').is(':checked'))
+  {
+    $('.service').each(function() {
+        console.log($(this).prop('name'));
+        if ($(this).is(':checked'))
+        {
+        filter[$(this).prop('name').toLowerCase()] = true;
+        }
+        });
+  }
+  console.log(filter);
+  if (all || caltex) parse.csv('csv/caltex.csv',
+      function(data) {initialize(data, 'images/marker_caltex.png')},
+      filter);
+  if (all || shell) parse.csv('csv/shell.csv',
+      function(data) {initialize(data, 'images/marker_shell.png')},
+      filter);
+  if (all || BP) parse.csv('csv/BP.csv',
+      function(data) {initialize(data, 'images/marker_bp.png')},
+      filter);
+  if (all || mobil) parse.csv('csv/mobil.csv',
+      function(data) {initialize(data, 'images/marker_711.png')},
+      filter);
 }
 
 
@@ -110,51 +110,51 @@ function initCSV()
 function initialize(petrol, markerImage) 
 {
 
-	var stations = [];
-	for (var i = 0; i < petrol.length; i++)
-	{
-		current = petrol[i];
-		//console.log(current['latitude']);
-		if (distance(current['latitude'], current['longitude']) < 10)
-		{
-			stations.push({
-					'location': new google.maps.LatLng(current['latitude'], 
-						current['longitude']),
-					'title': current['address']
-					});
-		}
-	}
-	google.maps.event.addListenerOnce(map, 'center_changed', function() {
-			pins = drop(stations, map, markerImage);
-			});
-	centerMap();
+  var stations = [];
+  for (var i = 0; i < petrol.length; i++)
+  {
+    current = petrol[i];
+    //console.log(current['latitude']);
+    if (distance(current['latitude'], current['longitude']) < 10)
+    {
+      stations.push({
+          'location': new google.maps.LatLng(current['latitude'],
+            current['longitude']),
+          'title': current['address']
+          });
+    }
+  }
+  google.maps.event.addListenerOnce(map, 'center_changed', function() {
+      pins = drop(stations, map, markerImage);
+      });
+  centerMap();
 }
 
 function pinClicks(pins)
 {
-	for (var i = 0; i < pins.length; i++)
-	{
-		google.maps.event.addListener(markers[i], 'click', function() {
-				console.log(this.getTitle());
-				});
-	}
+  for (var i = 0; i < pins.length; i++)
+  {
+    google.maps.event.addListener(markers[i], 'click', function() {
+        console.log(this.getTitle());
+        });
+  }
 }
 /*********************
  * Shiny Tab Clicking Panel Sliding...stuff.
  *******************/
 function selectTab(elem)
 {
-	if ($(elem).hasClass('Selected'))
-	{
-		$('#' + $(elem).data('show')).slideUp();
-		$(elem).removeClass('Selected');
-	} else {
-		console.log(elem);
-		$(elem).addClass('Selected');
-		$('#' + $(elem).data('hide')).slideUp();
-		$('#' + $(elem).data('show')).slideDown();
-		$('#' + $(elem).data('hide') + 'Tab').removeClass('Selected');
-	}
+  if ($(elem).hasClass('Selected'))
+  {
+    $('#' + $(elem).data('show')).slideUp();
+    $(elem).removeClass('Selected');
+  } else {
+    console.log(elem);
+    $(elem).addClass('Selected');
+    $('#' + $(elem).data('hide')).slideUp();
+    $('#' + $(elem).data('show')).slideDown();
+    $('#' + $(elem).data('hide') + 'Tab').removeClass('Selected');
+  }
 }
 
 /********************
@@ -163,10 +163,10 @@ function selectTab(elem)
  ********************/
 function clickSearch()
 {
-	if (pins)
-	{
-		clearPins(pins);
-		initCSV();
-	}
-	$('#dvServicesTab').click();
+  if (pins)
+  {
+    clearPins(pins);
+    initCSV();
+  }
+  $('#dvServicesTab').click();
 }
--- js/utils.js
+++ js/utils.js
@@ -1,29 +1,29 @@
 /**********************
- * Function to calculate distance from current location from stackoverflow
- * See answer http://stackoverflow.com/a/27943/453554 for more information.
- **********************/
+* Function to calculate distance from current location from stackoverflow
+* See answer http://stackoverflow.com/a/27943/453554 for more information.
+**********************/
 
 function distance(lat2, lon2)
 {
-	if (typeof(Number.prototype.toRad) === "undefined") {
-		  Number.prototype.toRad = function() {
-			      return this * Math.PI / 180;
-			        }
-	}
+  if (typeof(Number.prototype.toRad) === 'undefined') {
+    Number.prototype.toRad = function() {
+      return this * Math.PI / 180;
+    };
+  }
 
-	lat2 = lat2 * 1;
-	lon2 = lon2 * 1;
-	
-	var lat1 = userLoc["lat"];
-	var lon1 = userLoc["long"];
-	var R = 6371; // Radius of the earth in km
-	var dLat = (lat2-lat1).toRad();
-	var dLon = (lon2-lon1).toRad(); 
-	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-		        Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
-			        Math.sin(dLon/2) * Math.sin(dLon/2); 
-	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-	var d = R * c; //
-	return d;
+  lat2 = lat2 * 1;
+  lon2 = lon2 * 1;
+
+  var lat1 = userLoc['lat'];
+  var lon1 = userLoc['long'];
+  var R = 6371; // Radius of the earth in km
+  var dLat = (lat2 - lat1).toRad();
+  var dLon = (lon2 - lon1).toRad();
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return d;
 }
 
