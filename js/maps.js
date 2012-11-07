a8493d57298d219482ef0f54d714430b78cf5062
Peter Monus <pete.monus@gmail.com>
2012-11-07 13:29:21 +1100

    Added directional services

--- js/maps.js
+++ js/maps.js
@@ -3,19 +3,19 @@
  ****************/
 function initMap()
 {
-  var mapOptions =
-  {
-center: new google.maps.LatLng(-35.237, 149.085),
-        zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  return new google.maps.Map(document.getElementById('map_canvas'),
-      mapOptions);
+	var mapOptions = 
+	{
+        center: new google.maps.LatLng( -35.237,149.085),
+		zoom: 13,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	return new google.maps.Map(document.getElementById("map_canvas"),
+	mapOptions);
 }
 function centerMap()
 {
-  map.setCenter(new google.maps.LatLng(userLoc['lat'], userLoc['long']));
-
+	map.setCenter(new google.maps.LatLng(userLoc["lat"], userLoc["long"]));
+	
 }
 
 
@@ -24,28 +24,28 @@ function centerMap()
  ****************/
 var markers = [];
 function drop(stations, map, image) {
-  for (var i = 0; i < stations.length; i++) {
-    markers.push(new google.maps.Marker({
-position: (stations[i])['location'],
-title: (stations[i])['title'],
-map: map,
-draggable: false,
-clickable: true,
-icon: image,
-animation: google.maps.Animation.DROP
-}));
+	for (var i = 0; i < stations.length; i++) {
+		markers.push(new google.maps.Marker({
+			position: (stations[i])["location"],
+			title: (stations[i])["title"],
+			map: map,
+			draggable: false,
+			clickable: true,
+			icon: image,
+			animation: google.maps.Animation.DROP
+		}));
 
-}
+	}
 
-pinClicks(markers);
-return markers;
+	pinClicks(markers);
+	return markers;
 }
 
 function clearPins(pins)
 {
-  for (var i = 0; i < pins.length; i++)
-  {
-    pins[i].setMap(null);
-  }
-  markers = [];
+	for(var i = 0; i < pins.length; i++)
+	{
+		pins[i].setMap(null);
+	}
+	markers = [];
 }
--- js/parse.js
+++ js/parse.js
@@ -2,7 +2,7 @@
  * Parser constructor. OOPJS ALL UP IN THIS THING!
  * TODO: Make the base constructor actually do something useful?
  ***************/
-var parser = function() {this.types = ['csv'];};
+var parser = function(){this.types = ["csv"];};
 
 /****************
  * Add function to the parser object to parse CSV.
@@ -11,62 +11,56 @@ var parser = function() {this.types = ['csv'];};
  * TODO: Filters are functions for super filtery goodness?
  ****************/
 parser.prototype.csv = function(csvName, callback, filter)
-{
-  $.get(
-      csvName,
-      function(data) {dat = parsecsv(data, filter, callback);} ,
-      'text'
-      );
-};
+{	
+	$.get(csvName, function(data) {dat = parsecsv(data, filter, callback);}, "text");
+}
 
 /***********************
- * Filter by value, filter takes the form {'column header':boolean}
- * You can pass in any number of pairs,
- * and will only return true if they're all correct
+ * Filter by value, filter takes the form {"column header":boolean}
+ * You can pass in any number of pairs, and will only return true if they're all correct
  ***********************/
 boolFilter = function(data, filter)
 {
-  var containsVal = true;
-  for (var key in filter)
-  {
-    if (data[key] && (data[key].toLowerCase() == 'y') == filter[key])
-    {
-      containsVal = true;
-    } else { return false }
-  }
-  return containsVal;
-};
+	var containsVal = true;
+	for(var key in filter)
+	{
+		if(data[key] && (data[key].toLowerCase() == "y") == filter[key])
+		{
+			containsVal =  true;
+		} else { return false }
+	}
+	return containsVal;
+}
 
 
 /************************
  * The ACTUAL parser, splits stuff up, makes things super easy to use.
- * Returns an array of objects,
- * so we can iterate through easily and get the data we need.
+ * Returns an array of objects, so we can iterate through easily and get the data we need.
  ************************/
 
-parsecsv = function(data, filter, callBack) {
-  var dat = [];
-  data = data.replace(/\n/g, '');
-  var lines = data.split('\r');
-  var headers = lines[0].toLowerCase().split(',');
-  for (var i = 1; i < lines.length; i++)
-  {
-    var lineArr = lines[i].split(',');
-    var line = {};
-    if (lineArr.length > 1)
-    {
-      for (var l = 0; l < lineArr.length; l++)
-      {
-        line[headers[l]] = (lineArr[l].length > 0) ? lineArr[l] : ' ';
-      }
-      if (boolFilter(line, filter))
-      {
-        dat.push(line);
-      }
-    }
-  }
-  callBack(dat);
-};
+parsecsv = function(data, filter, callBack){
+	var dat = [];
+	data = data.replace(/\n/g, "");
+	var lines = data.split("\r");
+	var headers = lines[0].toLowerCase().split(',');
+	for(var i = 1; i < lines.length; i++)
+	{
+			var lineArr = lines[i].split(',');
+			var line = {};
+			if(lineArr.length > 1)
+			{
+				for(var l = 0; l < lineArr.length; l++)
+				{
+					line[headers[l]] = (lineArr[l].length > 0) ? lineArr[l] : " ";
+				}
+				if(boolFilter(line, filter))
+				{
+				dat.push(line);
+				}
+			}
+	}
+	callBack(dat);
+}
 
-//Create a global parse object so we can call parse.csv()
-var parse = new parser();
+
+var parse = new parser();//Create a global parse object so we can call parse.csv()
